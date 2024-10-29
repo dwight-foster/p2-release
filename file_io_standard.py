394410565448f2fb91593f9e83a82d791f002ab0
Wei Tang <wei@Weis-MacBook-Air-2.local>
2024-10-29 18:29:59 -0400

    initial commit

---
+++ file_io_standard.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+#
+# IU CSCI-B 455, Fall 2024
+# PRJ2: Spam detection
+#
+
+from pathlib import Path
+
+if __name__ == "__main__":
+    INPUT_DIR = Path('./data').resolve()
+    OUTPUT_DIR = Path('./').resolve()
